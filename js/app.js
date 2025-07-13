d0578a138ff697eabd5e3e1146cbdfdb5d97d595
Marcelo Antonio Pereira Marcolino <marcelo.marcolino.dev@gmil.com>
2025-07-13 01:59:05 -0300

    Conversão de Temperatura

--- js/app.js
+++ js/app.js
@@ -110,7 +110,23 @@ function verificarParOuImpar(numero){
     }
 }
 
-// Exemplo de usso
+// Exemplo de uso
 let numero = 7;
 let resultadoParOuImpar = verificarParOuImpar(numero);
 console.log(resultadoParOuImpar); // Saída esperada: Ímpar
+
+// Desafio 5
+function converterTemperatura(valor, escala) {
+    if (escala.toLowerCase() === 'celsius') {
+        return (valor * 9/5) + 32; // Celsius para Fahrenheit
+    } else if (escala.toLowerCase() === 'fahrenheit') {
+        return (valor - 32) * 5/9; // Fahrenheit para Celsius
+    } else {
+        return "Escala inválida";
+    }
+}
+
+// Exemplo de uso
+let temperaturaCelsius = 25;
+let temperaturaFahrenheit = converterTemperatura(temperaturaCelsius, 'celsius');
+console.log(temperaturaFahrenheit); // Saída esperada: 77
